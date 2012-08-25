0333da34fd2b7846b8b955f59c170e21e1ba5f9c
joepfarley <admin@joepfarley.com>
2012-08-24 22:52:23 -0500

    started regex rawPhraser

--- Phraser.py
+++ Phraser.py
@@ -1,9 +1,17 @@
-from Dice import roll,d4,d6,d8,d10,d12,d100
+from Dice import roll,d4,d6,d8,d10,d12,d100,array,help
 
-while True:
-	try:
-		x=input(">>> ")
-		print x
-	except SyntaxError:print "stop fuckin' around"
-	except NameError:print "stop fuckin' around"
-	except ZeroDivisionError:print "Can't divide by zero dummy"
+def rawPhraser():
+	x=input(">>> ")
+	#some if then statement that uses regex to pick out d for 1dsomething or other  
+
+
+def phraser():
+	while True:
+		try:
+			x=input(">>> ")
+			print x
+		except SyntaxError:print "type help() for assistance"
+		except NameError:print "type help() for assistance"
+		except ZeroDivisionError:print "type help() for assistance"
+
+phraser()
