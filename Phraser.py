28711f0f7d0c140488fcf3fcd3e6622cd9a50595
joepfarley <admin@joepfarley.com>
2012-08-30 01:15:37 +0000

    detleted useless function. PANCAKES

--- Phraser.py
+++ Phraser.py
@@ -1,10 +1,5 @@
 from Dice import roll,d4,d6,d8,d10,d12,d100,array,help
-
-def rawPhraser():
-	x=raw_input(">>> ")
-	if re.x ==  'test':
-		print "test"
-
+import re
 
 def phraser():
 	while True:
@@ -15,7 +10,7 @@ def phraser():
 		except NameError:print "type help() for assistance"
 		except ZeroDivisionError:print "type help() for assistance"
 
-rawPhraser()
+phraser()
 
 
 
